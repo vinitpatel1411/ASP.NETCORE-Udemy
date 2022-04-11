2249372aaf354e9eb92dfa14f580b7429dcb4359
Vinit Patel <vinit1411@gmail.com>
2022-04-11 15:13:51 +0530

    Adding Updated Pages

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,28 @@
+﻿using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Actor:IEntityBase
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Profile Picture URL")]
+        [Required(ErrorMessage ="Profile picture URL is Required")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
+        public string FullName { get; set; }
+        [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
+        public string Bio { get; set; }
+
+        
+        public List<Actor_Movie> Actor_Movies { get; set; }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Profile Picture URL")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+        [Display(Name = "Biography")]
+        public string Bio { get; set; }
+
+        //Relationsips
+        public List<Movie> Movies { get; set; }
+    }
+}
